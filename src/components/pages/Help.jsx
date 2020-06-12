c0b62db3d9bac27c286e055c4662246db8d86412
SirGoPadawan <orangehardcore0@gmail.com>
2020-06-13 04:14:59 +1000

    добавил карту гугл

--- src/components/pages/Help.jsx
+++ src/components/pages/Help.jsx
@@ -1,29 +1,28 @@
 import React from 'react';
-import PropTypes from 'prop-types'
 import GoogleMapReact from 'google-map-react';
 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
+const AnyReactComponent = ({ text }) => <div className="marker-map">{text}</div>;
+
 
 export default class SimpleMap extends React.Component {
-  getDefaultProps() {
+ static defaultProps = {
     center: {
-      lat: 59.95;
-      lng: 30.33
-    }
-    zoom: 11
-  };
-
+      lat: 48.461916,
+      lng: 135.086769
+    },
+    zoom: 15
+  }
   render() {
     return (
-      <div style={{ height: '100vh', width: '100%' }}>
+      <div style={{ height: '80vh', width: '100%' }}>
         <GoogleMapReact
-          bootstrapURLKeys={{ key: AIzaSyDsXmjyMzJJTlBbK7TpDjZ2bQjeyzsARrw}}
+          bootstrapURLKeys={{ key: "AIzaSyDsXmjyMzJJTlBbK7TpDjZ2bQjeyzsARrw"}}
           defaultCenter={this.props.center}
           defaultZoom={this.props.zoom}
         >
           <AnyReactComponent
-            lat={59.955413}
-            lng={30.337844}
+            lat={48.461916}
+            lng={135.086769}
             text="ФОГСТРИМ"
           />
         </GoogleMapReact>
